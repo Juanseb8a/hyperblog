58ad302e64dea0a77821cfd592fc4349c3642ad0
Sebastian Ochoa <ochoajuan105@gmail.com>
2022-12-24 08:35:41 -0500

    Matplotlib basics - Getting data and values

--- practice1.py
+++ practice1.py
@@ -24,4 +24,31 @@ sample_data = pd.read_csv('sample_data.csv')
 # print(sample_data.column_c)
 
 #  Getting a specific value 
-print(sample_data.column_c.iloc[0])
+# print(sample_data.column_c.iloc[0])
+
+# Plotting columns of a data set
+# plt.plot(sample_data.column_a, sample_data.column_b, 'o')
+# plt.plot(sample_data.column_a, sample_data.column_c)
+
+# print(sample_data.column_c)
+# print(sample_data.column_b)
+
+# plt.legend(["this is a vs b","this is a vs c"])
+# plt.show()
+
+#  Compare the population grwoth in the US and China
+# Getting all data in a variable
+data = pd.read_csv('countries.csv')
+print(data)
+
+afghanistan = data[data.country == 'Afghanistan']
+china = data[data.country == "China"]
+
+print(afghanistan.population)
+
+plt.plot(afghanistan.year, afghanistan.population/afghanistan.population.iloc[0]*100)
+plt.plot(china.year, china.population/china.population.iloc[0]*100)
+plt.legend(["This is Afghanistan", "This is China"])
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.show()
