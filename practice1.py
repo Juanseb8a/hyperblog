186eecd3963c5113fbd4f6bb2ae4ef75f3a47e20
Sebastian Ochoa <ochoajuan105@gmail.com>
2022-12-24 15:54:02 -0500

    Matplotlib Tutorial 1 - Terminado

--- practice1.py
+++ practice1.py
@@ -3,33 +3,52 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import style
 
+
+
+# Matplotlib Tutorial (Part )
+
+
+
+# Matplotlib Tutorial (Part 1)
+#  -------------------------------------------
 # plt.style.use('ggplot')
-plt.xkcd()
+# # plt.xkcd()
 
-dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# x_indexes = np.arange(len(dev_x))
+# width = 0.25
 
-dev_y = [38496, 42000, 46752, 49320, 53200, 
-56000, 62316, 64928, 67317, 68748, 73752]
+# dev_y = [38496, 42000, 46752, 49320, 53200, 
+# 56000, 62316, 64928, 67317, 68748, 73752]
 
-py_dev_y = [45375, 48876, 53850, 57287, 
-63016, 65998, 70003, 70000, 71496, 75370, 83640]
+# py_dev_y = [45375, 48876, 53850, 57287, 
+# 63016, 65998, 70003, 70000, 71496, 75370, 83640]
 
-js_dev_y = [37810, 43515,46823,49293,53437,
-56373, 62375, 66674, 68745, 68746, 74583]
+# js_dev_y = [37810, 43515,46823,49293,53437,
+# 56373, 62375, 66674, 68745, 68746, 74583]
 
-plt.plot(dev_x, dev_y, color='#444444', linestyle='--', marker='.', label='All Devs')
-plt.plot(dev_x, py_dev_y, color='#5a7d9a', linewidth=3, marker='o', label='Python')
-plt.plot(dev_x, js_dev_y, color='#123456', linestyle='--', label="Javascript")
+# # plt.plot(dev_x, dev_y, color='#444444', linestyle='--', marker='.', label='All Devs')
+# # plt.plot(dev_x, py_dev_y, color='#5a7d9a', linewidth=3, marker='o', label='Python')
+# # plt.plot(dev_x, js_dev_y, color='#123456', linestyle='--', label="Javascript")
 
-plt.xlabel('Ages')
-plt.ylabel('Median Salary (USD)')
-plt.title('Median Salary (USD) by Age')
+# plt.bar(x_indexes-width, dev_y, width=width, color="#444444", label="All Devs")
+# plt.bar(x_indexes, py_dev_y, width=width, color="#008fd5", label="Python")
+# plt.bar(x_indexes+width, js_dev_y, width=width, color="#e5ae38", label="JavaScript")
 
-plt.legend()
+# plt.legend()
 
-# plt.grid(True)
-plt.tight_layout()
-plt.show()
+# plt.xticks(ticks=x_indexes, labels=dev_x)
+
+# plt.title('Median Salary (USD) by Age')
+# plt.xlabel('Ages')
+# plt.ylabel('Median Salary (USD)')
+
+# # plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+#  -------------------------------------------------------------------------
+
 
 # Basics of plotting
 # x = [1,  2,  3]
