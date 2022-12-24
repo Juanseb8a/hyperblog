3052cc660d389b0ddb9def204cdda6058459f971
Sebastian Ochoa <ochoajuan105@gmail.com>
2022-12-24 07:40:36 -0500

    Getting a column and a specific value in Pandas dataframe

--- practice1.py
+++ practice1.py
@@ -2,19 +2,26 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-
 # Basics of plotting
-x = [1, 2, 3]
-y = [1, 4, 9]
-z = [10, 5, 0]
+# x = [1, 2, 3]
+# y = [1, 4, 9]
+# z = [10, 5, 0]
 
-plt.plot(x,z)
-plt.plot(x,y)
+# plt.plot(x,z)
+# plt.plot(x,y)
 
-plt.title("test plot")
-plt.xlabel("x")
-plt.ylabel("y and z")
+# plt.title("test plot")
+# plt.xlabel("x")
+# plt.ylabel("y and z")
 
-plt.legend(["this is z", "this is y"])
-plt.show()
+# plt.legend(["this is z", "this is y"])
+# plt.show()
 
+#  Basics of getting data
+sample_data = pd.read_csv('sample_data.csv')
+
+#  Getting a column 
+# print(sample_data.column_c)
+
+#  Getting a specific value 
+print(sample_data.column_c.iloc[0])
