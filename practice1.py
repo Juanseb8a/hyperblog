a18c8030826b6ba4a0af3ef9ed7bfa1245867f7d
Sebastian Ochoa <ochoajuan105@gmail.com>
2022-12-24 07:26:26 -0500

    Basics of plotting in Matplotlib

--- practice1.py
+++ practice1.py
@@ -1,3 +1,20 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+
+
+# Basics of plotting
+x = [1, 2, 3]
+y = [1, 4, 9]
+z = [10, 5, 0]
+
+plt.plot(x,z)
+plt.plot(x,y)
+
+plt.title("test plot")
+plt.xlabel("x")
+plt.ylabel("y and z")
+
+plt.legend(["this is z", "this is y"])
+plt.show()
 
