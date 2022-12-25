ba24b35ee63a0a8386e1eaed8869279bdb305155
Sebastian Ochoa <ochoajuan105@gmail.com>
2022-12-25 09:56:20 -0500

    Matplotlib tutorial Part 2

--- practice1.py
+++ practice1.py
@@ -2,13 +2,44 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import style
+import csv
+from collections import Counter
 
 
 
-# Matplotlib Tutorial (Part )
 
 
+# Matplotlib Tutorial (Part 2)
+# data = pd.read_csv('data.txt')
+# ids = data['Responder_id']
+# lang_responses = data['LanguagesWorkedWith']
 
+# language_counter = Counter()
+
+# # with open('data.txt') as csv_file:
+# #     csv_reader = csv.DictReader(csv_file)
+# #     language_counter = Counter()
+
+# for response in lang_responses:
+#     language_counter.update(response.split(';'))
+    
+# languages = []
+# popularity = []
+
+# for item in language_counter.most_common(15):
+#     languages.append(item[0])
+#     popularity.append(item[1])
+
+# languages.reverse()
+# popularity.reverse()
+
+# plt.barh(languages, popularity)
+
+# plt.title("Most popular Languages")
+# plt.ylabel("Number of People Who Use")
+
+# plt.tight_layout()
+# plt.show()
 # Matplotlib Tutorial (Part 1)
 #  -------------------------------------------
 # plt.style.use('ggplot')
@@ -66,7 +97,7 @@ from matplotlib import style
 # plt.show()
 
 #  Basics of getting data
-sample_data = pd.read_csv('sample_data.csv')
+# sample_data = pd.read_csv('sample_data.csv')
 
 #  Getting a column 
 # print(sample_data.column_c)
