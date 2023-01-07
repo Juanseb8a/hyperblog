e0eb84ce9af6a1562456df7038f1706f229f46f3
Sebastian Ochoa <ochoajuan105@gmail.com>
2023-01-07 10:23:14 -0500

    Up to Pivoting lesson

--- peskills.py
+++ peskills.py
@@ -108,3 +108,137 @@ df = pd.DataFrame([[1,np.nan,2],[2,3,5],[np.nan,4,6]])
 
 # # conditional selection
 # print(data[data['Number of speakers']<5000])
+
+#  Drop column row
+# data = pd.read_csv('data.csv')
+# # print(data.head(n=5))
+
+# # print(type(data))
+
+# #  removing the first row
+# # data.drop(data.index[0], inplace=True)
+
+# #  removing the first two rows
+# data.drop(data.index[:2], inplace=True)
+# print(data)
+
+# # drop column named latitude
+# data.drop(['Latitude'], axis = 1, inplace = True)
+
+#  Subsetting and Indexing
+data = pd.read_csv('data.csv')
+# print(data)
+
+# selecting all entries from index 2 onwards
+# print(data[6:])
+
+# #  select entries at even index locations
+# print(data[::2])
+
+# #  specify the ranges of rows and columns TO SELECT 
+# print(data.iloc[0:8,0:7])
+
+# #  select all columns for rows of index values 0 and 10
+# print(data.iloc[[0,10], : ])
+
+#  isolate rows and columns
+# print(data[['Countries', 'Name in English']][4:8])
+
+# isolate by row names
+# print(data[data.Countries == "Italy"])
+
+#  isolate by row labels
+# x = data.loc[data['Countries'].isin(['Italy','Germany'])]
+# print(x.head(15))
+
+#  replace "Number of speakers" with "Number"
+df = data[:]
+df = pd.DataFrame(data=df)
+# df.rename(columns={'Number of Speakers' : 'Number'}, inplace = True)
+# print(df.head(5))
+# print(data[(data.Countries=="India") & (data.Degree of endangerment == "Vulnerable")])
+
+# df.drop(['Name in English'], axis = 1)
+# df = df.drop(['Description of the location','Name in French', 'Name in English', 'Name in Spanish', 'Latitude', 'Longitude'], axis = 1)
+# df = df[df.Countries == "India"]
+# df.rename(columns={df.columns[7] : "Number"}, inplace = True)
+# df = df[(df.Countries == "India") & (df.Number < 1751489.0)]
+
+# print(df.columns)
+# print(df)
+
+# #  sort in descending order
+# large = data.sort_values(by='Number of speakers', ascending=False)
+# print(large.head(n=10))
+
+# # sort in descending order
+# small = data.sort_values(by='Number of speakers', ascending = True)
+# print(small.head(n=10))
+
+# # grouping by
+# group_by = data.groupby(['Degree of endangerment']).count()
+# print(group_by)
+
+# group on basis of two qualitative columns
+# bygroup_CnS = data.groupby(['Countries', 'Degree of endangerment'])
+# print(data.head(10))
+# print(bygroup_CnS.count())
+
+# group number of speañers by group labels (row names) from countries
+# x = data['Number of speakers'].groupby(data['Countries'])
+# print(x.head(n=10))
+
+# Lambda functions
+# Identify which of the languages have fewer than 5k speakers
+
+# data['vfew'] = data['Number of speakers'].apply(lambda x: x <= 5000)
+# # print(data.head(n=6))
+
+# print(data['Degree of endangerment'].value_counts())
+
+# #  tabulate our data on basis of two columns
+# x = pd.crosstab(data["Country codes alpha 3"], data["Degree of endangerment"], margins=True)
+# print(x.head(4))
+# # print(data.columns)
+
+# def perConvert(var):
+#     return var/float(var[-1])
+
+# x = pd.crosstab(data["Country codes alpha 3"], data["Degree of endangerment"], margins = True).apply(perConvert, axis = 1)
+# print(x.head(3))
+
+# dstack = data.stack()
+# print(dstack)
+
+# patient = pd.DataFrame({'FirstName' : ['Bill', 'Jane'], 'LastName' : ['Shakespeare', 'Austen'], 'BloodType' : ['o+', 'B+'],
+#                         'wt' : [85, 62]})
+# print(patient)
+
+#  melting
+#  use melt()
+# you have a data that has one or more columns that are identifier variables
+# while all other columns are considered measured variables
+
+# print(pd.melt(patient, id_vars = ['FirstName','LastName'], var_name = 'measurements'))
+
+# x = data[0:15]
+# print(x.head(3))
+
+#  drop id
+# x = x.drop('ID', axis=1)
+# print(x.head(4))
+
+#  drop multiple columns
+# x1 = data[0:15]
+# # x1 = data.drop(columns=['Latitude', 'Longitude'])
+# print(x1.head(4))
+
+# melted = pd.melt(x1, id_vars = ['Latitude', 'Longitude'], value_name = "No")
+# print(melted)
+
+# Pivoting
+data1 = pd.read_csv('ranking_table.csv')
+print(data1.head(5))
+
+print(data1.pivot_table(index='power_index', columns='progress', values='rank', aggfunc='sum'))
+print(data1.columns)
